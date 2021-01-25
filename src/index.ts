0fde321736939404090a6619ca92ba1d172daea7
maxscharwath <maxscharwath@gmail.com>
2021-01-26 00:07:15 +0100

    use of streamboard-sdk package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
-import PluginAbstract from "./PluginAbstract";
+import PluginAbstract from "streamboard-sdk/dist/PluginAbstract";
+import {EventName} from "streamboard-sdk";
 
 class SimplePlugin extends PluginAbstract {
-  protected async init(): Promise<void> {
-    console.log("plugin connected");
-  }
+    constructor() {
+        super();
+    }
+    protected async init(): Promise<void> {
+        console.log("plugin connected");
+    }
 }
 
 const p = new SimplePlugin();
-p.on("test", d => {
-  console.log("Received=>", d);
+let i = 0;
+p.onAction("ch.studimax.ch.simple-plugin.test", (ctx, event, data) => {
+    ctx.setText(`${++i}`);
+    switch (event) {
+        case EventName.ADDED:
+            ctx.setText("Hello");
+            ctx.setColor("#1452bc");
+            break
+        case EventName.CLICK:
+            ctx.setColor("#14bc30");
+            break;
+        case EventName.DBLCLICK:
+            ctx.setImage("https://media.tenor.com/images/0ca6f8a1d31646286b94671f0cd0f5a4/tenor.gif");
+            ctx.setColor("#bc141a");
+            break;
+    }
 });
