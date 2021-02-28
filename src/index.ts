e7bc6a2261c94f485eb59e9fb3f7551d18b7ae62
maxscharwath <maxscharwath@gmail.com>
2021-02-28 18:44:13 +0100

    use the new SDK

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,21 @@ class SimplePlugin extends PluginAbstract {
 
     protected async init(): Promise<void> {
         console.log("plugin connected");
+        this.on("connected", context => {
+            let i = 0;
+            context.setText("...");
+            context.setColor("#1452bc");
+            context.on(EventName.CLICK, () => {
+                if (++i % 2 == 0) {
+                    context.setText("Hello");
+                    context.setColor("#14bc30");
+                } else {
+                    context.setText("World");
+                    context.setColor("#cf0000");
+                }
+            });
+        });
     }
 }
 
-const p = new SimplePlugin();
-let i = 0;
-p.onAction("test", (ctx, event) => {
-    switch (event) {
-        case EventName.ADDED:
-            ctx.setText("...");
-            ctx.setColor("#1452bc");
-            break
-        case EventName.CLICK:
-            if (++i % 2 == 0) {
-                ctx.setText("Hello");
-                ctx.setColor("#14bc30");
-            } else {
-                ctx.setText("World");
-                ctx.setColor("#cf0000");
-            }
-            break;
-    }
-});
+new SimplePlugin();
