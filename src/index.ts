c99578bf8396949081925ae9fe79301f59067dca
maxscharwath <maxscharwath@gmail.com>
2021-04-14 20:36:58 +0200

    use streamboard-sdk 1.3.0

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
 import StreamBoardSDK from "streamboard-sdk";
 
+
 const sdk = new StreamBoardSDK();
 console.log("plugin connected");
+
 sdk.onConnection(context => {
-    let i = 0;
-    context.setText("...");
-    context.setColor("#1452bc");
-    context.onClick(() => {
-        if (++i % 2 == 0) {
-            context.setText("Hello");
-            context.setColor("#14bc30");
-        } else {
-            context.setText("World");
-            context.setColor("#cf0000");
-        }
+    console.log(context);
+    switch (context.action){
+        case 'test-a':{
+            let i = 0;
+            context.setText("...");
+            context.setColor("#1452bc");
+            context.onPressUp(({pressDuration}) => {
+                context.setText(`${pressDuration}`);
+                if (++i % 2 == 0) {
+                    context.setColor("#14bc30");
+                } else {
+                    context.setColor("#cf0000");
+                }
+            });
+        }break;
+        case 'test-b':{
+            let i = 0;
+            setInterval(()=>{
+                context.setText(`${i++}`);
+                context.setColor("#1452bc");
+            },100);
+        }break;
+    }
+    context.onSettings(() => {
+        return [
+            {
+                label: "Test",
+                action: "test",
+                type: "input_select",
+                items: []
+            }
+        ]
     });
 });
+
+sdk.ready();
