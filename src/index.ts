d1505982e0e8960f0b307f4de66cb20d347518c6
maxscharwath <maxscharwath@gmail.com>
2021-06-13 15:24:24 +0200

    use streamboard-sdk ^1.6.0

--- src/index.ts
+++ src/index.ts
@@ -4,51 +4,47 @@ import StreamBoardSDK from "streamboard-sdk";
 const sdk = new StreamBoardSDK();
 console.log("plugin connected");
 
-sdk.setConfigForm('test-a',[
+sdk.setActionConfig('test-a', [
     {
         label: "Test",
-        action: "test",
+        key: "test",
         type: "input_select",
-        default:()=>'value_20',
-        items: ()=>Array.from(Array(100),(i,index)=>({label:`Label ${index}`,value:`value_${index}`}))
+        default: () => 'value_20',
+        items: () => Array.from(Array(100), (i, index) => ({label: `Label ${index}`, value: `value_${index}`}))
     },
     {
         type: 'input_checkbox',
         label: "Boolean",
-        action: 'check',
+        key: 'check1',
     },
     {
         type: 'input_checkbox',
         label: "Boolean",
-        action: 'check',
-        default:()=>true
+        key: 'check2',
+        default: () => true
     }
 ])
 
-sdk.onContext(context => {
+sdk.onContext('test-a', context => {
     console.log(context);
-    switch (context.action){
-        case 'test-a':{
-            let i = 0;
-            context.setText("...");
-            context.setColor("#1452bc");
-            context.onPressUp(({pressDuration}) => {
-                context.setText(`${pressDuration}`);
-                if (++i % 2 == 0) {
-                    context.setColor("#14bc30");
-                } else {
-                    context.setColor("#cf0000");
-                }
-            });
-        }break;
-        case 'test-b':{
-            let i = 0;
-            setInterval(()=>{
-                context.setText(`${i++}`);
-                context.setColor("#1452bc");
-            },100);
-        }break;
-    }
+    let i = 0;
+    context.setText("...");
+    context.setColor("#1452bc");
+    context.onPressUp(({pressDuration}) => {
+        context.setText(`${pressDuration}`);
+        if (++i % 2 == 0) {
+            context.setColor("#14bc30");
+        } else {
+            context.setColor("#cf0000");
+        }
+    });
 });
-
+sdk.onContext('test-b', context => {
+    console.log(context);
+    let i = 0;
+    setInterval(() => {
+        context.setText(`${i++}`);
+        context.setColor("#1452bc");
+    }, 100);
+});
 sdk.ready();
